8d64cbf828056f63b8e0b10ebb233aff61c66590
HarshaSalih <harshasalih2000@gmail.com>
2023-06-23 22:58:34 +0530

    add

--- src/main/java/com/example/Task_manager/controller/TaskController.java
+++ src/main/java/com/example/Task_manager/controller/TaskController.java
@@ -1,9 +1,24 @@
 package com.example.Task_manager.controller;
 
+import com.example.Task_manager.model.Tasks;
+import com.example.Task_manager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-
+@RequestMapping("/tasks")
 public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping
+    public ResponseEntity<Tasks> createTask(@RequestBody Tasks tasks) {
+        Tasks savedTask = taskRepository.save(tasks);
+        return ResponseEntity.ok(savedTask);
+    }
 }
